aaeb81289a7caf937db0c01956c5e5ddb4b27e81
LouisG1977 <luisgonzales272@gmail.com>
2020-06-30 13:51:25 -0600

    Blissify

--- bliss_davinci.mk
+++ bliss_davinci.mk
@@ -21,13 +21,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+TARGET_BOOT_ANIMATION_RES := 1440
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_davinci
+PRODUCT_NAME := bliss_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
 PRODUCT_MANUFACTURER := Xiaomi
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="davinci"
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Bliss
+export BLISS_BUILDTYPE=OFFICIAL
+#TARGET_USES_LAWNCHAIR := true
